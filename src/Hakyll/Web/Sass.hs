fd24f48c0aa5e007978e1b76972a43ba5a967ace
Braden Walters <vc@braden-walters.info>
2015-08-21 23:30:34 +0200

    Initial commit

---
+++ src/Hakyll/Web/Sass.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module: Hakyll.Web.Sass
+-- Copyright: (C) 2015 Braden Walters
+-- License: MIT (see LICENSE file)
+-- Maintainer: Braden Walters <vc@braden-walters.info>
+-- Stability: experimental
+-- Portability: ghc
+
+module Hakyll.Web.Sass where
