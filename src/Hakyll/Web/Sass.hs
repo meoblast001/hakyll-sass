627875a17fa9a7ad9e22456aa52ed208052b06e5
Braden Walters <vc@braden-walters.info>
2015-08-22 01:11:54 +0200

    Determine whether file is SASS or SCSS

--- src/Hakyll/Web/Sass.hs
+++ src/Hakyll/Web/Sass.hs
@@ -14,12 +14,24 @@ import Hakyll.Core.Compiler
 import Hakyll.Core.Item
 import System.IO.Unsafe
 import Text.Sass.Compilation
+import Text.Sass.Options
 
 -- | Compiles a SASS file into CSS.
 sassCompiler :: Compiler (Item String)
 sassCompiler = do
   bodyStr <- itemBody <$> getResourceBody
-  resultOrErr <- unsafeCompiler (compileString bodyStr def)
-  case resultOrErr of
-    Left sassError -> fail (unsafePerformIO $ errorMessage sassError)
-    Right result -> makeItem result
+  extension <- getUnderlyingExtension
+  let mayOptions = selectFileType def extension
+  case mayOptions of
+    Just options -> do
+      resultOrErr <- unsafeCompiler (compileString bodyStr options)
+      case resultOrErr of
+        Left sassError -> fail (unsafePerformIO $ errorMessage sassError)
+        Right result -> makeItem result
+    Nothing -> fail "File type must be .scss or .sass."
+
+-- | Use the file extension to determine whether to use indented syntax.
+selectFileType :: SassOptions -> String -> Maybe SassOptions
+selectFileType options ".scss" = Just $ options { sassIsIndentedSyntax = False }
+selectFileType options ".sass" = Just $ options { sassIsIndentedSyntax = True }
+selectFileType _ _ = Nothing
